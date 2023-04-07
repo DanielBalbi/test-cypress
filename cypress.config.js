ddc664b1f7e5808bcec91339c39dc0bf7bd0b452
DanielBalbi <111795829+DanielBalbi@users.noreply.github.com>
2023-04-07 20:30:21 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
       baseUrl: "https://www.emaras.com.ar/",
-      video: false,
+      video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
