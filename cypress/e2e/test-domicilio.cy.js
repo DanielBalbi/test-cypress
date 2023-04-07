bfb3d0124d422e78e89ac951978f743621c07fa5
DanielBalbi <111795829+DanielBalbi@users.noreply.github.com>
2023-04-07 20:26:29 -0300

    Update test-domicilio.cy.js

--- cypress/e2e/test-domicilio.cy.js
+++ cypress/e2e/test-domicilio.cy.js
@@ -12,7 +12,7 @@ describe('Login de Usuario en Emaras', ()=>{
     it('Escribo mi usuario y clave', ()=>{
         /////////////////////////////  Pantalla de Login 
 
-       cy.logueo()
+       cy.mi_login()
        
         /////////////////////////////  Pantalla de Selección de Consorcios
         /// click sobre el edicio
